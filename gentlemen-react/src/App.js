21d14f6c868c9b4fe2c76078cb3577cf730e7bf4
Javierfg86 <Javierfg86@outlook.es>
2022-05-30 23:31:51 +0200

    Add changes at react files

--- gentlemen-react/src/App.js
+++ gentlemen-react/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Gentleman } from './components/Gentleman/gentleman';
+import { Info } from './components/Info/info';
+import { Button } from './components/Button/button';
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <>
+      <Info></Info>
+      <Gentleman></Gentleman>
+      <Button></Button>
+    </>
+  );
 }
 
 export default App;
